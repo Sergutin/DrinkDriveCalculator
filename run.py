8280718dcfaf7d1025d4b6eecda45deb57317f17
Sergutin <igor@sergutin.com>
2023-01-13 17:24:33 +0000

    Fix input issue that didnt accept correct input from the 1st attempt

--- run.py
+++ run.py
@@ -1,77 +1,80 @@
 from datetime import datetime, timedelta
-x = datetime.now().strftime("%H:%M %Y-%m-%d")
 
-#Standard drinks number in a drink in a pub-----------
+# Standard drinks number in a drink in a pub-----------
 
 standard_drinks_pub = {'Pints of lager, 3% (568ml)': 1.3,
-'Pints of lager, 4.5% (568ml)': 2.05,
-'Bottles of lager, 4.5% (330ml)': 1,
-'Pints of stout, 4.2% (568ml)': 2.05,
-'Pints of cider, 4.5% (568ml)': 2.05,
-'Bottles of cider, 4.5% (330ml)': 1,
-'Bottles of alcopop, 4% (330ml)': 1.2,
-'Glasses of white wine, 12.5% (150ml)': 1.5,
-'Bottles of white wine, 12.5% (750ml)': 7.5,
-'Glasses of red wine, 12.5% (150ml)': 1.5,
-'Bottles of red wine, 12.5% (750ml)': 7.5,
-'Glasses of sparkling wine, 11.5% (125ml)': 1,
-'Measures of spirits, 40% (35.5ml)': 1}
+                        'Pints of lager, 4.5% (568ml)': 2.05,
+                        'Bottles of lager, 4.5% (330ml)': 1,
+                        'Pints of stout, 4.2% (568ml)': 2.05,
+                        'Pints of cider, 4.5% (568ml)': 2.05,
+                        'Bottles of cider, 4.5% (330ml)': 1,
+                        'Bottles of alcopop, 4% (330ml)': 1.2,
+                        'Glasses of white wine, 12.5% (150ml)': 1.5,
+                        'Bottles of white wine, 12.5% (750ml)': 7.5,
+                        'Glasses of red wine, 12.5% (150ml)': 1.5,
+                        'Bottles of red wine, 12.5% (750ml)': 7.5,
+                        'Glasses of sparkling wine, 11.5% (125ml)': 1,
+                        'Measures of spirits, 40% (35.5ml)': 1}
 
-#Standard drinks number in a drink at home-------------
+# Standard drinks number in a drink at home-------------
 
 standard_drinks_home = {'Cans of lager, 4.5% (500ml)': 1.8,
-'Bottles of lager, 4.5% (330ml)': 1,
-'Cans of stout, 4.2% (500ml)': 1.8,
-'Cans of cider, 4.5% (500ml)': 1.8,
-'Bottles of cider, 4.5% (330ml)': 1,
-'Bottles of alcopop, 4% (330ml)': 1.2,
-'Glasses of white wine, 12.5% (150ml)': 1.5,
-'Bottles of white wine, 12.5% (750ml)': 7.5,
-'Glasses of red wine, 12.5% (150ml)': 1.5,
-'Bottles of red wine, 12.5% (750ml)': 7.5,
-'Glasses of sparkling wine, 11.5% (125ml)': 1,
-'Measures of spirits, 40% (35.5ml)': 1}
+                        'Bottles of lager, 4.5% (330ml)': 1,
+                        'Cans of stout, 4.2% (500ml)': 1.8,
+                        'Cans of cider, 4.5% (500ml)': 1.8,
+                        'Bottles of cider, 4.5% (330ml)': 1,
+                        'Bottles of alcopop, 4% (330ml)': 1.2,
+                        'Glasses of white wine, 12.5% (150ml)': 1.5,
+                        'Bottles of white wine, 12.5% (750ml)': 7.5,
+                        'Glasses of red wine, 12.5% (150ml)': 1.5,
+                        'Bottles of red wine, 12.5% (750ml)': 7.5,
+                        'Glasses of sparkling wine, 11.5% (125ml)': 1,
+                        'Measures of spirits, 40% (35.5ml)': 1}
 
-#Welcome--------------------------------------------------
+# Welcome--------------------------------------------------
 
-print("Wellcome to Drink and Drive Calculator!!")
+print("Welcome to Drink and Drive Calculator!")
 
 print("\n")
 
-#Gender input---------------------------------------------
+# Gender input---------------------------------------------
 
 gender = input("Are you male or female? m/f \n")
-while gender != 'm' or gender != 'f':
+print(f'You entered: {gender}')
+while gender != 'm' and gender != 'f':
     print('Please use "m" or "f" to reply \n')
     gender = input("Are you male or female? m/f \n")
-    if gender == "m" or gender == "f":
-        print(f'You entered {gender}')
-        break
+    # if gender == "m" or gender == "f":
+    #     print(f'You entered: {gender}')
+    #     break
 
 print("\n")
 
 drinks_number = 0
 process_time_pub = 0
 
-#Drink place input----------------------------------------
+# Drink place input----------------------------------------
 
 place = input("Where were you Drinking: pub / home? \n")
-while place != 'pub' or place != 'home':
+print(f'You entered: {place}')
+while place != 'pub' and place != 'home':
     print('Please use "pub" or "home" to reply \n')
     place = input("Where were you Drinking: pub / home? \n")
-    
-    #Drink choice in a pub--------------------------------
+    # if place == "pub" or place == "home":
+    #     print(f'You entered: {place}')
+
+    # Drink choice in a pub--------------------------------
 
     if place == 'pub':
         drinks_pub = int(input("""\n What drinks did you have? Enter a number: \n
-        1. Pints of lager, 3% (568ml) 
+        1. Pints of lager, 3% (568ml)
         2. Pints of lager, 4.5% (568ml)
         3. Bottles of lager, 4.5% (330ml)
         4. Pints of stout, 4.2% (568ml)
         5. Pints of cider, 4.5% (568ml)
         6. Bottles of cider, 4.5% (330ml)
         7. Bottles of alcopop, 4% (330ml)
-        8. Glasses of white wine, 12.5% (150ml) 
+        8. Glasses of white wine, 12.5% (150ml)
         9. Bottles of white wine, 12.5% (750ml)
         10. Glasses of red wine, 12.5% (150ml)
         11. Bottles of red wine, 12.5% (750ml)
@@ -80,8 +83,8 @@ while place != 'pub' or place != 'home':
         """))
         print(f'You entered: {drinks_pub}')
         print('\n')
-    
-        #Number of drinks choice in a pub--------------------------------
+
+        # Number of drinks choice in a pub--------------------------------
 
         try:
             drinks_number = int(input("How many drinks did you have? \n"))
@@ -92,16 +95,14 @@ while place != 'pub' or place != 'home':
             print(f'You entered: {drinks_number}')
             print('\n')
 
-        #Process time formula, pub
+        # Process time formula, pub
 
-        # print(drinks_number)
-        # print(list(standard_drinks_pub.values())[drinks_pub - 1])
         process_time_pub = drinks_number * list(standard_drinks_pub.values())[drinks_pub - 1]
         time_pub = datetime.now() + timedelta(hours=process_time_pub)
         print(f'The alcohol should have left your system in: {process_time_pub} hours at {time_pub}')
         break
-    
-    #Drink choice at home--------------------------------
+
+    # Drink choice at home--------------------------------
 
     elif place == 'home':
         drinks_home = int(input("""\n What drinks did you have? Enter a number: \n
@@ -111,7 +112,7 @@ while place != 'pub' or place != 'home':
         4. Cans of cider, 4.5% (500ml)
         5. Bottles of cider, 4.5% (330ml)
         6. Bottles of alcopop, 4% (330ml)
-        7. Glasses of white wine, 12.5% (150ml) 
+        7. Glasses of white wine, 12.5% (150ml)
         8. Bottles of white wine, 12.5% (750ml)
         9. Glasses of red wine, 12.5% (150ml)
         10. Bottles of red wine, 12.5% (750ml)
@@ -120,8 +121,8 @@ while place != 'pub' or place != 'home':
         """))
         print(f'You entered: {drinks_home}')
         print('\n')
-        
-        #Number of drinks choice at home--------------------------------
+
+        # Number of drinks choice at home--------------------------------
 
         try:
             drinks_number = int(input("How many drinks did you have? \n"))
@@ -132,13 +133,8 @@ while place != 'pub' or place != 'home':
             print(f'You entered: {drinks_number}')
             print('\n')
 
-        #Process time formula, home
-        
-        # print(drinks_number)
-        # print(list(standard_drinks_home.values())[drinks_home - 1])
+        # Process time formula, home
+
         process_time_home = drinks_number * list(standard_drinks_home.values())[drinks_home - 1]
         time_home = datetime.now() + timedelta(hours=process_time_home)
         print(f'The alcohol should have left your system in: {process_time_home} hours at {time_home}')
-        break
-
-print("\n")
--- test.py
+++ test.py
@@ -1,7 +1,22 @@
-from datetime import datetime, timedelta
-x = datetime.now().strftime("%H:%M %Y-%m-%d")
-print(x)
+# gender = input("Are you male or female? m/f \n")
+# while gender != 'm' or gender != 'f':
+#     print('Please use "m" or "f" to reply \n')
+#     gender = input("Are you male or female? m/f \n")
+#     if gender == "m" or gender == "f":
+#         print(f'You entered {gender}')
+#         break
 
-y = 3
-date = datetime.now() + timedelta(hours=y)
-print(date)
+gender = input("Are you male or female? m/f \n")
+while gender != 'm' and gender != 'f':
+    print('Please use "m" or "f" to reply \n')
+    gender = input("Are you male or female? m/f \n")
+    if gender == "m" or gender == "f":
+        print(f'You entered {gender}')
+        break
+
+
+
+    # if gender == "m" or gender == "f":
+    #     print(f'You entered: {gender}')
+    #     break
+    
